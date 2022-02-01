ba68bfaa3472ee3acb5ca13d16c6fcca983ba7bf
Reden4005 <olus81_81@o2.pl>
2022-02-01 20:13:55 +0100

    Changes according CR

--- apps/site-e2e/src/integration/routing/routing.spec.ts
+++ apps/site-e2e/src/integration/routing/routing.spec.ts
@@ -1,34 +1,41 @@
 import { AppRoute } from "@patronage-web/shared";
+import { generatePath } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 
 describe("Base routing", () => {
+  const buildPath = (to: AppRoute, id?: string) => {
+    return generatePath(`${AppRoute.Presentation}/${to}`, { id });
+  };
   it(`should display <${AppRoute.Home}> page`, () => {
     cy.visit("/").url().should("include", "/");
   });
 
   it(`should display <${AppRoute.Presentation}> page`, () => {
-    cy.visit("/presentation").url().should("include", "/presentation");
+    cy.visit(AppRoute.Presentation).url().should("include", AppRoute.Presentation);
   });
 
   it(`should display <${AppRoute.Dashboard}> page`, () => {
-    cy.visit("/dashboard").url().should("include", "/dashboard");
+    cy.visit(AppRoute.Dashboard).url().should("include", AppRoute.Dashboard);
   });
 
   it(`should display <${AppRoute.AddPresentation}> page`, () => {
-    cy.visit("/presentation/add").url().should("include", "/presentation/add");
+    const path = buildPath(AppRoute.AddPresentation);
+    cy.visit(path).url().should("include", path);
   });
 
   it(`should display <${AppRoute.EditPresentation}> page`, () => {
     const Id = uuidv4();
-    cy.visit(`/presentation/edit/${Id}`).url().should("include", `/presentation/edit/${Id}`);
+    const path = buildPath(AppRoute.EditPresentation, Id);
+    cy.visit(path).url().should("include", path);
   });
 
   it(`should display <${AppRoute.PresentationForExternalUser}> page`, () => {
     const Id = uuidv4();
-    cy.visit(`/presentation/${Id}`).url().should("include", `/presentation/${Id}`);
+    const path = buildPath(AppRoute.PresentationForExternalUser, Id);
+    cy.visit(path).url().should("include", path);
   });
 
   it(`should display <${AppRoute.NotFound}> page`, () => {
-    cy.visit("/dashboard/something").url().should("include", "/dashboard/something");
+    cy.visit(`${AppRoute.Dashboard}/something`).url().should("include", `${AppRoute.Dashboard}/something`);
   });
 });
--- apps/site/src/app/utils/routing/index.tsx
+++ apps/site/src/app/utils/routing/index.tsx
@@ -1,5 +1,5 @@
 import { AppRoute } from "@patronage-web/shared";
-import { Suspense } from "react";
+import React, { Suspense, SuspenseProps } from "react";
 import { Outlet, RouteObject, useRoutes } from "react-router-dom";
 
 const createChildrenRoute = (route: AppRoute, component: JSX.Element): RouteObject => {
@@ -9,18 +9,22 @@ const createChildrenRoute = (route: AppRoute, component: JSX.Element): RouteObje
 const createRoute = (
   route: AppRoute,
   component: JSX.Element,
-  fallback: NonNullable<React.ReactNode> | null,
+  fallback: SuspenseProps["fallback"],
   children?: RouteObject[]
 ): RouteObject => {
+  let wrappedChildren: RouteObject[] | undefined;
+  if (children) {
+    wrappedChildren = children.map(child => {
+      return {
+        path: child.path,
+        element: <Suspense fallback={fallback}>{child.element}</Suspense>
+      };
+    });
+  }
   return {
     path: route,
-    element: (
-      <Suspense fallback={fallback}>
-        {component}
-        <Outlet />
-      </Suspense>
-    ),
-    children
+    element: <Suspense fallback={fallback}>{component}</Suspense>,
+    children: wrappedChildren
   };
 };
 
@@ -28,11 +32,19 @@ export const Routing: React.FC = () => {
   return useRoutes([
     createRoute(AppRoute.Home, <h1>Home page</h1>, <h1>...loading</h1>),
     createRoute(AppRoute.Dashboard, <h1>Dashboard page</h1>, <h1>...loading</h1>),
-    createRoute(AppRoute.Presentation, <h1>Presentation page</h1>, <h1>...loading</h1>, [
-      createChildrenRoute(AppRoute.AddPresentation, <h1>Add presentation page</h1>),
-      createChildrenRoute(AppRoute.EditPresentation, <h1>Edit presentation page</h1>),
-      createChildrenRoute(AppRoute.PresentationForExternalUser, <h1>Page for external user</h1>)
-    ]),
+    createRoute(
+      AppRoute.Presentation,
+      <>
+        <h1>Presentation page</h1>
+        <Outlet />
+      </>,
+      <h1>...loading</h1>,
+      [
+        createChildrenRoute(AppRoute.AddPresentation, <h1>Add presentation page</h1>),
+        createChildrenRoute(AppRoute.EditPresentation, <h1>Edit presentation page</h1>),
+        createChildrenRoute(AppRoute.PresentationForExternalUser, <h1>Page for external user</h1>)
+      ]
+    ),
     createRoute(AppRoute.NotFound, <h1>You should not be here...</h1>, <h1>...loading</h1>)
   ]);
 };
