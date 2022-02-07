18d683b3c7e3b26d19b8735bff159508c7a91013
A.Wasilewski <wasilewski.adrian.64@gmail.com>
2022-02-07 20:52:58 +0100

    chore(actioncard story): refactor storybook to work with the localizedLink

--- libs/ui/src/stories/action-card/action-card.stories.tsx
+++ libs/ui/src/stories/action-card/action-card.stories.tsx
@@ -1,16 +1,19 @@
 import { StarsImage } from "@patronage-web/features-feedback";
-import { ActionCard, ActionCardProps } from "@patronage-web/shared";
+import { ActionCard, ActionCardProps, AppRoute } from "@patronage-web/shared";
 import { ComponentMeta } from "@storybook/react";
+import { BrowserRouter } from "react-router-dom";
 
 export const ActionCardStory: React.FC<ActionCardProps> = ({ description, image, button }) => (
-  <ActionCard description={description} image={image} button={button} />
+  <BrowserRouter>
+    <ActionCard description={description} image={image} button={button} />
+  </BrowserRouter>
 );
 
 export default {
   title: "ActionCard",
   component: ActionCardStory,
   args: {
-    button: { variant: "contained", text: "Lorem ipsum" },
+    button: { variant: "contained", text: "Lorem ipsum", navigateTo: [AppRoute.Presentation, AppRoute.AddPresentation] },
     description:
       "Always remember in the jungle there’s a lot of they in there, after you overcome they, you will make it to paradise."
   },
