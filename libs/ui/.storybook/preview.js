656eaeaafc2bb222b812625001175bb8957dc0e3
Paweł Wojdas <paw.wojdas@gmail.com>
2022-02-16 09:58:58 +0100

    feat(basebutton): add storybook

--- libs/ui/.storybook/preview.js
+++ libs/ui/.storybook/preview.js
@@ -1,5 +1,16 @@
-import { ThemeProvider } from "@patronage-web/shared";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider as MuiThemeProvider } from "@mui/material/styles";
+import { dark, light } from "@patronage-web/shared";
 import { MemoryRouter } from "react-router-dom";
+
+export const parameters = {
+  backgrounds: {
+    values: [
+      { name: "light", value: light.palette.background.default },
+      { name: "dark", value: dark.palette.background.default }
+    ]
+  }
+};
 
 export const decorators = [
   Story => (
@@ -7,9 +18,14 @@ export const decorators = [
       <Story />
     </MemoryRouter>
   ),
-  Story => (
-    <ThemeProvider>
-      <Story />
-    </ThemeProvider>
-  )
+  (Story, context) => {
+    const currentTheme = context.globals?.backgrounds?.value === dark.palette.background.default ? dark : light;
+
+    return (
+      <MuiThemeProvider theme={currentTheme}>
+        <CssBaseline />
+        <Story {...context} />
+      </MuiThemeProvider>
+    );
+  }
 ];
