5088dd3e59f8d1605f65acecc48c2e4380c43f92
piotrrawski <piotr.rawski@gmail.com>
2022-02-16 16:35:14 +0100

    refactor(language-selector): changes for better logic and naming

--- libs/features/feedback/src/components/language-button/index.tsx
+++ libs/features/feedback/src/components/language-button/index.tsx
@@ -5,18 +5,19 @@ import { useTranslation } from "react-i18next";
 import { useSearchParams } from "react-router-dom";
 
 export const LanguageButton = () => {
-  const { i18n } = useTranslation();
+  const { t, i18n } = useTranslation();
   const [, setSearchParams] = useSearchParams();
 
-  const isDefaultLanguage = i18n.language?.length && i18n.language !== SupportedLanguage.En ? `?lang=${i18n.language}` : "";
+  const isOtherLang = i18n.language?.length && i18n.language !== SupportedLanguage.En;
+  const langQueryString = isOtherLang ? `?lang=${i18n.language}` : "";
 
   const handleChange = (event: React.MouseEvent<HTMLElement>, value: SupportedLanguage) => {
     changeLanguage(i18n, value);
   };
 
   useEffect(() => {
-    setSearchParams(isDefaultLanguage);
-  }, [i18n.language, isDefaultLanguage, setSearchParams]);
+    setSearchParams(langQueryString);
+  }, [i18n.language, langQueryString, setSearchParams]);
 
   return (
     <ToggleButtonGroup color='primary' size='small' value={i18n.language} onChange={handleChange} exclusive>
