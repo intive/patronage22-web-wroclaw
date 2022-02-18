0f4319011049176456e36e3eec756f2a83006177
piotrrawski <piotr.rawski@gmail.com>
2022-02-18 11:14:41 +0100

    style(language-selector-storybook): improve namespace files

--- libs/ui/.storybook/i18n.js
+++ libs/ui/.storybook/i18n.js
@@ -1,3 +1,3 @@
 import { loadI18n, TranslationNamespace } from "@patronage-web/shared";
 
-loadI18n({ env: "development", ns: [TranslationNamespace.Feedback, TranslationNamespace.Common] });
+loadI18n({ env: "development", ns: [TranslationNamespace.Ui, TranslationNamespace.Feedback, TranslationNamespace.Common] });
