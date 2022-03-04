8e0a61a03629c7d42730118cdc9838ebd6994328
Paweł Wojdas <paw.wojdas@gmail.com>
2022-03-04 09:22:25 +0100

    refactor(layout): change layout folder structure

--- libs/shared/src/components/index.ts
+++ libs/shared/src/components/index.ts
@@ -3,6 +3,7 @@ export * from "./base-button";
 export * from "./basic-info";
 export * from "./footer";
 export * from "./language-button";
+export * from "./layout";
 export * from "./loader";
 export * from "./localized-link";
 export * from "./navbar";
--- libs/shared/src/components/layout/index.tsx
+++ libs/shared/src/components/layout/index.tsx
@@ -1,13 +1,13 @@
-import { Footer, FooterProps } from "../../components/footer";
-import { Navbar, NavbarProps } from "../../components/navbar";
+import { Footer, FooterProps } from "../footer";
+import { Navbar, NavbarProps } from "../navbar";
 import * as Styled from "./style";
 
-export interface BasicLayoutProps {
+export interface LayoutProps {
   navbarConfig: NavbarProps["config"];
   footerConfig: FooterProps["config"];
 }
 
-export const BasicLayout: React.FC<BasicLayoutProps> = ({ children, navbarConfig, footerConfig }) => (
+export const Layout: React.FC<LayoutProps> = ({ children, navbarConfig, footerConfig }) => (
   <Styled.LayoutBox>
     <Navbar color='inherit' position='sticky' config={navbarConfig} />
     <Styled.ContentBox>{children}</Styled.ContentBox>
