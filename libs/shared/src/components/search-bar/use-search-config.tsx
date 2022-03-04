b4b743f8473e5fa5a173d7f2110de347f4f027fa
Reden4005 <olus81_81@o2.pl>
2022-03-04 12:28:24 +0100

    Changes accorging CR

--- libs/shared/src/components/search-bar/use-search-config.tsx
+++ libs/shared/src/components/search-bar/use-search-config.tsx
@@ -2,22 +2,24 @@ import { useFeatureName } from "../../hooks";
 import { FeatureName, FeedbackRoute } from "../../types";
 import { ConfigSearchBar } from "./types";
 
-const SEARCH_BAR_CONFIG: Record<Exclude<FeatureName, FeatureName.ExternalFeedback>, ConfigSearchBar> = {
-  [FeatureName.Feedback]: {
-    searchKey: "title",
-    allResultsPage: FeedbackRoute.Dashboard,
-    singleResultPage: FeedbackRoute.EditPresentation
-  },
-  // TODO replace by proper values when available
-  [FeatureName.Default]: {
-    searchKey: "title",
-    allResultsPage: FeedbackRoute.Dashboard,
-    singleResultPage: FeedbackRoute.EditPresentation
-  }
-};
-
 export const useSearchConfig = () => {
   const featureName = useFeatureName();
 
-  return featureName !== FeatureName.ExternalFeedback ? SEARCH_BAR_CONFIG[featureName] : SEARCH_BAR_CONFIG[FeatureName.Default];
+  // TODO replace by proper values when available
+  const DEFAULT_CONFIG: ConfigSearchBar = {
+    searchKey: "title",
+    allResultsPage: FeedbackRoute.Dashboard,
+    singleResultPage: FeedbackRoute.EditPresentation
+  };
+
+  const CONFIGS: Partial<Record<FeatureName, ConfigSearchBar>> = {
+    [FeatureName.Feedback]: {
+      searchKey: "title",
+      allResultsPage: FeedbackRoute.Dashboard,
+      singleResultPage: FeedbackRoute.EditPresentation
+    },
+    [FeatureName.Default]: DEFAULT_CONFIG
+  };
+
+  return CONFIGS[featureName] ?? DEFAULT_CONFIG;
 };
