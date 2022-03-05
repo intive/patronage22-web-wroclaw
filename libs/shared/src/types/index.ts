5abe69f1daa38607863aa217931093558e3e181e
Anna Kozik <anna.kozik@yahoo.pl>
2022-03-05 21:45:48 +0100

    refactor(share dialog): add types to snackbar notifications

--- libs/shared/src/types/index.ts
+++ libs/shared/src/types/index.ts
@@ -1,5 +1,6 @@
 export * from "./app-routes";
 export * from "./form-field-type";
 export * from "./http-status";
+export * from "./notification-type";
 export * from "./theme-modes";
 export * from "./translations";
