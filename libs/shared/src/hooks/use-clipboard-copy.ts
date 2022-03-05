a0fd798a99b2ccf930127b24891f207f82d96683
Anna Kozik <anna.kozik@yahoo.pl>
2022-03-05 21:39:27 +0100

    refactor(share dialog): add types to snackbar notifications

--- libs/shared/src/hooks/use-clipboard-copy.ts
+++ libs/shared/src/hooks/use-clipboard-copy.ts
@@ -4,22 +4,25 @@ import { v4 as uuidv4 } from "uuid";
 
 import { addNotification } from "../data/features";
 import { AppDispatch } from "../data/types";
-import { TranslationNamespace } from "../types";
+import { NotificationType, TranslationNamespace } from "../types";
 
 export const useClipboardCopy = () => {
   const { t } = useTranslation([TranslationNamespace.Common]);
   let message: string;
   let id: string;
+  let type: NotificationType;
   const dispatch = useDispatch<AppDispatch>();
   const onSuccess = () => {
     message = t("linkCopied", { ns: TranslationNamespace.Common });
     id = uuidv4();
-    dispatch(addNotification([{ id, message }]));
+    type = NotificationType.SuccessSnackbar;
+    dispatch(addNotification([{ id, message, type }]));
   };
   const onFail = () => {
     message = t("linkCopyingFailed", { ns: TranslationNamespace.Common });
     id = uuidv4();
-    dispatch(addNotification([{ id, message }]));
+    type = NotificationType.ErrorSnackbar;
+    dispatch(addNotification([{ id, message, type }]));
   };
 
   const copyFunction = (text: string) => {
