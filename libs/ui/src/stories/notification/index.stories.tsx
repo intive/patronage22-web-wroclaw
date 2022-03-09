e8b33fff1403ff9af5fd9c4475bb26a399ac8de7
Anna Kozik <anna.kozik@yahoo.pl>
2022-03-09 11:11:27 +0100

    refactor(share dialog): add changes to notification storybook

--- libs/ui/src/stories/notification/index.stories.tsx
+++ libs/ui/src/stories/notification/index.stories.tsx
@@ -1,22 +1,30 @@
 import { Notification, NotificationType } from "@patronage-web/shared";
 import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { useTranslation } from "react-i18next";
+import { v4 as uuidv4 } from "uuid";
 
 export default {
   title: "Notification",
   component: Notification,
-  args: {
-    id: "123",
-    message: "sample message",
-    type: NotificationType.Info
-  },
   argTypes: {
     type: {
+      control: "radio",
       defaultValue: NotificationType.Info
+    },
+    id: {
+      control: "none"
+    },
+    message: {
+      control: "none"
     }
   }
 } as ComponentMeta<typeof Notification>;
 
-const NotificationTemplate: ComponentStory<typeof Notification> = ({ id, message, type }) => {
+const NotificationTemplate: ComponentStory<typeof Notification> = (args: { type: NotificationType }) => {
+  const { t } = useTranslation();
+  const id = uuidv4();
+  const message = t("sample message");
+  const { type } = args;
   return <Notification id={id} message={message} type={type} key={id} />;
 };
 
