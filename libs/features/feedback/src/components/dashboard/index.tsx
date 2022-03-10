1897ea73e84d1871bebea3c815ae1e3483b54957
piotrrawski <piotr.rawski@gmail.com>
2022-03-10 11:28:18 +0100

    refactor(dashboard): destructure presentation in dashboard component

--- libs/features/feedback/src/components/dashboard/index.tsx
+++ libs/features/feedback/src/components/dashboard/index.tsx
@@ -1,5 +1,4 @@
 import { Grid } from "@mui/material";
-import { Presentation } from "@patronage-web/shared";
 import { presentations } from "@patronage-web/shared-data";
 
 import * as Styled from "./styled";
@@ -8,15 +7,9 @@ import { DashboardTile } from "./tile";
 export const Dashboard = () => {
   return (
     <Styled.DashboardTileGrid container spacing={1}>
-      {Object.values(presentations).map((presentation: Presentation) => (
-        <Grid item xs={12} sm={6} md={4} lg={3} key={presentation.id}>
-          <DashboardTile
-            id={presentation.id}
-            isPublic={presentation.isPublic}
-            title={presentation.title}
-            description={presentation.description}
-            status={presentation.status}
-          />
+      {Object.values(presentations).map(({ id, isPublic, title, description, status }) => (
+        <Grid item xs={12} sm={6} md={4} lg={3} key={id}>
+          <DashboardTile id={id} isPublic={isPublic} title={title} description={description} status={status} />
         </Grid>
       ))}
     </Styled.DashboardTileGrid>
