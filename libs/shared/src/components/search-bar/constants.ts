b2dce80f8542efecbe40da76bbf03390b0d28dd5
Reden4005 <olus81_81@o2.pl>
2022-03-11 12:57:25 +0100

    docs(search_config): docs corrected

--- libs/shared/src/components/search-bar/constants.ts
+++ libs/shared/src/components/search-bar/constants.ts
@@ -1,17 +1,18 @@
-/**
- * @argument minMath: The minimum number of chars in search string to trigger search
- * @argument limit: indicates the number of items to be return
- * @argument offset: indicates the number of items that should be skipped
- * @argument SEARCH_MAX_LENGTH: maximum number of characters that can be entered into input
- * @argument maxValidationLength: @argument SEARCH_MAX_LENGTH reduced by 1 to show error message
- */
-
 const SEARCH_MAX_LENGTH = 30;
 
-export const SEARCH_CONFIG = {
+/**
+ * @property {Object} SEARCH_CONFIG - The default values for searching functionality
+ * @property {number} SEARCH_CONFIG.limit - Indicates the number of items to be return
+ * @property {number} SEARCH_CONFIG.offset - Indicates the number of items that should be skipped
+ * @property {number} SEARCH_CONFIG.minMatch - The minimum number of chars in search string to trigger search
+ * @property {number} SEARCH_CONFIG.SEARCH_MAX_LENGTH - Maximum number of characters that can be entered into input
+ * @property {number} SEARCH_CONFIG.maxValidationLength - SEARCH_MAX_LENGTH reduced by 1 to show error message
+ */
+
+export const SEARCH_CONFIG = Object.freeze({
   limit: 5,
   offset: 0,
   minMatch: 3,
   maxLength: SEARCH_MAX_LENGTH,
   maxValidationLength: SEARCH_MAX_LENGTH - 1
-};
+});
