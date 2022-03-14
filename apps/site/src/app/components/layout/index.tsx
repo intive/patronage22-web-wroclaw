74285b855f9f6ee9b7550d83e8063c90316a9e98
Anna Kozik <anna.kozik@yahoo.pl>
2022-03-14 13:07:00 +0100

    fix(notifications): move Notifications component to Layout component in site section

--- apps/site/src/app/components/layout/index.tsx
+++ apps/site/src/app/components/layout/index.tsx
@@ -1,14 +1,29 @@
 import { Layout as FeedbackLayout } from "@patronage-web/features-feedback";
-import { FeatureName, useFeatureName } from "@patronage-web/shared";
+import { FeatureName, Notifications, useFeatureName } from "@patronage-web/shared";
 
 import { Layout as DefaultLayout } from "./default-layout";
 
 export const Layout: React.FC = ({ children }) => {
   const layouts = {
-    [FeatureName.Feedback]: <FeedbackLayout>{children}</FeedbackLayout>,
+    [FeatureName.Feedback]: (
+      <FeedbackLayout>
+        {children}
+        <Notifications />
+      </FeedbackLayout>
+    ),
     // TODO replace with proper external layout when will be ready
-    [FeatureName.ExternalFeedback]: <FeedbackLayout>{children}</FeedbackLayout>,
-    [FeatureName.Default]: <DefaultLayout>{children}</DefaultLayout>
+    [FeatureName.ExternalFeedback]: (
+      <FeedbackLayout>
+        {children}
+        <Notifications />
+      </FeedbackLayout>
+    ),
+    [FeatureName.Default]: (
+      <DefaultLayout>
+        {children}
+        <Notifications />
+      </DefaultLayout>
+    )
   };
 
   return layouts[useFeatureName()];
