039783621ba2b747f1174042f94393ec2ef7d5e6
piotrrawski <piotr.rawski@gmail.com>
2022-03-16 11:08:03 +0100

    refactor(dashboard): create styled component for grid item

--- libs/features/feedback/src/components/dashboard/index.tsx
+++ libs/features/feedback/src/components/dashboard/index.tsx
@@ -1,4 +1,3 @@
-import { Grid } from "@mui/material";
 import { presentations } from "@patronage-web/shared-data";
 
 import * as Styled from "./styled";
@@ -6,11 +5,11 @@ import { DashboardTile } from "./tile";
 
 export const Dashboard = () => {
   return (
-    <Styled.DashboardTileGrid container spacing={1}>
+    <Styled.DashboardTileGrid container>
       {Object.values(presentations).map(({ id, isPublic, title, description, status }) => (
-        <Grid item xs={12} sm={6} md={4} lg={3} key={id}>
+        <Styled.TileGrid item key={id}>
           <DashboardTile id={id} isPublic={isPublic} title={title} description={description} status={status} />
-        </Grid>
+        </Styled.TileGrid>
       ))}
     </Styled.DashboardTileGrid>
   );
--- libs/features/feedback/src/components/dashboard/styled.ts
+++ libs/features/feedback/src/components/dashboard/styled.ts
@@ -4,3 +4,21 @@ import { styled } from "@mui/material/styles";
 export const DashboardTileGrid = styled(Grid)(({ theme }) => ({
   padding: theme.spacing(2)
 }));
+
+export const TileGrid = styled(Grid)(({ theme }) => ({
+  [theme.breakpoints.up("xs")]: {
+    width: "100%"
+  },
+  [theme.breakpoints.up("sm")]: {
+    width: "50%"
+  },
+  [theme.breakpoints.up("md")]: {
+    width: "33%"
+  },
+  [theme.breakpoints.up("lg")]: {
+    width: "25%"
+  },
+  [theme.breakpoints.up("xl")]: {
+    width: "20%"
+  }
+}));
