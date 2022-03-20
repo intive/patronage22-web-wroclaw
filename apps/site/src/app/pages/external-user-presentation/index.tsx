bae5cd2b7053c5b8ba6ecf3fef2061273cc75d6b
Anna Kozik <anna.kozik@yahoo.pl>
2022-03-20 19:36:50 +0100

    feature(current question view): create dummy form and external presentation page

--- apps/site/src/app/pages/external-user-presentation/index.tsx
+++ apps/site/src/app/pages/external-user-presentation/index.tsx
@@ -1,16 +1,35 @@
+import { Button, Typography } from "@mui/material";
 import { CurrentQuestionView } from "@patronage-web/features-feedback";
+import { Timer } from "@patronage-web/shared";
 import { ExternalPresentationMock } from "@patronage-web/shared-data";
+import { useState } from "react";
 
 const ExternalUserPresentationPage: React.FC = () => {
-  const { questions } = ExternalPresentationMock;
-  const question = questions[0];
+  const { questions, timer } = ExternalPresentationMock;
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleSubmit = () => setIsSubmit(true);
+  const handleBtnClick = () => {
+    setQuestionIndex(questionIndex + 1);
+    setIsSubmit(false);
+  };
+
+  const view = (
+    <>
+      {!isSubmit ? (
+        <CurrentQuestionView question={questions[questionIndex]} onSubmit={handleSubmit} />
+      ) : (
+        <Button onClick={handleBtnClick}>Live Results</Button>
+      )}
+      <Timer initialTimeMsec={timer * 1000} onTimeElapsed={handleBtnClick} label='' key={questionIndex} />
+    </>
+  );
 
   return (
     // TODO - replace when proper data will be delivered
-    <>
-      <h1>Presentation page for external user</h1>
-      <CurrentQuestionView question={question} />
-    </>
+    // <h1>Presentation page for external user</h1>
+    questionIndex < questions.length ? view : <Typography>Thanks!</Typography>
   );
 };
 
--- libs/features/feedback/src/views/current-question-view/index.tsx
+++ libs/features/feedback/src/views/current-question-view/index.tsx
@@ -1,28 +1,50 @@
-import { Form, FormFieldProps, FormFieldType } from "@patronage-web/shared";
-import { ExternalQuestion } from "@patronage-web/shared-data";
+import { Button, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField, Typography } from "@mui/material";
+import { ExternalQuestion, QuestionType } from "@patronage-web/shared-data";
+import { useState } from "react";
 
 export interface CurrentQuestionViewProps {
   question: ExternalQuestion;
+  onSubmit: () => void;
 }
 
-export const CurrentQuestionView: React.FC<CurrentQuestionViewProps> = ({ question }) => {
+export const CurrentQuestionView: React.FC<CurrentQuestionViewProps> = ({ question, onSubmit }) => {
   // eslint-disable-next-line unused-imports/no-unused-vars
   const { id, number, title, type, answers, defaultAnswer } = question;
-  const validationSchema = {};
-  const fields: FormFieldProps[] = [
-    { type: FormFieldType.Text, name: "field1", variant: "filled" },
-    { type: FormFieldType.Textarea, name: "field2", variant: "outlined" }
-  ];
-  const handleSubmit = () => {};
+
+  const [usersAnswer, setUsersAnswer] = useState("");
+
+  const options = answers?.map(answer => (
+    <FormControlLabel value={answer} control={<Radio />} label={answer} key={answers.indexOf(answer)} />
+  ));
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsersAnswer((event.target as HTMLInputElement).value);
+  };
+
+  const handleSubmit = () => {
+    onSubmit();
+    console.log(usersAnswer);
+  };
+
+  const answersField = {
+    [QuestionType.Closed]: (
+      <RadioGroup value={usersAnswer} onChange={handleChange}>
+        {options}
+      </RadioGroup>
+    ),
+    [QuestionType.Open]: <TextField placeholder='Your answer' variant='standard' fullWidth onChange={handleChange} />
+  };
 
   return (
-    <Form
-      title={title}
-      validationSchema={validationSchema}
-      fields={fields}
-      onSubmit={handleSubmit}
-      showSubmitButton
-      submitButtonText='Submit'
-    />
+    <FormControl>
+      <FormLabel>
+        <Typography>{number}</Typography>
+        <Typography>{title}</Typography>
+      </FormLabel>
+      {answersField[type]}
+      <Button onClick={handleSubmit} variant='contained' disabled={usersAnswer === ""}>
+        Submit
+      </Button>
+    </FormControl>
   );
 };
