2d489c6f1ebe32defaf8399d938d4ecf7e7aa0b2
A.Wasilewski <wasilewski.adrian.64@gmail.com>
2022-03-21 21:59:52 +0100

    refactor(presentations-slice): add types for presentation and question, update types for queries

--- libs/shared/src/data/features/feedback/presentations/presentations-slice.ts
+++ libs/shared/src/data/features/feedback/presentations/presentations-slice.ts
@@ -1,24 +1,25 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-import { FeedbackSliceName } from "../types";
+import { FeedbackSliceName, Presentation } from "../types";
 
 export const presentations = createApi({
   reducerPath: FeedbackSliceName.Presentations,
+  // TODO: replace baseUrl with valid url to backend
   baseQuery: fetchBaseQuery({ baseUrl: "/some-url/presentations" }),
   endpoints: builder => ({
-    getPresentations: builder.query({
+    getPresentations: builder.query<Presentation[], void>({
       query: () => "/"
     }),
-    getPresentation: builder.query({
+    getPresentation: builder.query<Presentation, Presentation["id"]>({
       query: id => `/${id}`
     }),
-    addPresentation: builder.mutation({
+    addPresentation: builder.mutation<void, Presentation>({
       query: presentation => ({ url: "/", method: "POST", body: presentation })
     }),
-    changePresentation: builder.mutation({
+    changePresentation: builder.mutation<void, Presentation>({
       query: presentation => ({ url: `/${presentation.id}`, method: "PUT", body: presentation })
     }),
-    deletePresentation: builder.mutation({
+    deletePresentation: builder.mutation<void, Presentation["id"]>({
       query: id => ({ url: `/${id}`, method: "DELETE" })
     })
   })
--- libs/shared/src/data/features/feedback/types.ts
+++ libs/shared/src/data/features/feedback/types.ts
@@ -1,3 +1,24 @@
 export enum FeedbackSliceName {
   Presentations = "presentations"
 }
+
+export interface Question {
+  id: string;
+  content: string;
+  answers: string[];
+  answer: string;
+}
+
+export interface Presentation {
+  id: string;
+  name: string;
+  description: string;
+  email: string;
+  startTime: Date;
+  currentTime: Date;
+  timer: number;
+  link: string;
+  status: "ACTIVE" | "COMPLETED" | "DRAFT";
+  isPublic: boolean;
+  questions: Question[];
+}
