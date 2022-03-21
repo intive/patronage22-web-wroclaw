4fbc17fa9fb9f678f55fea8210f27c567e352649
Reden4005 <67143818+Reden4005@users.noreply.github.com>
2022-03-21 13:26:01 +0100

    fix(form): fix onSubmit in form (#67)
    fix onSubmit in form

--- libs/shared/src/components/form/index.tsx
+++ libs/shared/src/components/form/index.tsx
@@ -1,6 +1,7 @@
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Typography } from "@mui/material";
 import { isEqual } from "lodash-es";
+import { BaseSyntheticEvent } from "react";
 import { FieldValues, FormProvider, SubmitErrorHandler, SubmitHandler, useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 import * as yup from "yup";
@@ -47,16 +48,15 @@ export const Form: React.FC<FormProps> = ({
 
   const methods = useForm<FieldValues>({
     resolver: yupResolver(schema),
-    mode: "onChange",
-    reValidateMode: "onChange"
+    mode: "onChange"
   });
 
   const currentValues = methods.getValues();
   const previousValues = usePrevious(currentValues);
 
-  const handleSubmit = () => {
+  const handleSubmit = (event: BaseSyntheticEvent) => {
     if (onSubmit) {
-      methods.handleSubmit(onSubmit, onError)();
+      methods.handleSubmit(onSubmit, onError)(event);
     }
   };
 
@@ -91,7 +91,7 @@ export const Form: React.FC<FormProps> = ({
 
   return (
     <FormProvider {...methods}>
-      <Styled.Form className={className} onChange={handleFormChange}>
+      <Styled.Form className={className} onChange={handleFormChange} onSubmit={handleSubmit}>
         {title && <Typography variant='h3'>{title}</Typography>}
 
         {renderFields()}
