1322f48019e4b1cd2e02b7d449fe4224ad292dfc
Reden4005 <olus81_81@o2.pl>
2022-03-28 11:27:19 +0200

    docs(previouspagebutton): add a comment

--- libs/features/feedback/src/components/previous-page-button/index.tsx
+++ libs/features/feedback/src/components/previous-page-button/index.tsx
@@ -7,18 +7,19 @@ import { useNavigate } from "react-router-dom";
 import * as Styled from "./styled";
 
 export const PreviousPageButton = () => {
+  // TODO replace routing logic with redux-first-routing when will be ready
   const navigate = useNavigate();
 
   return (
     <Styled.PageToggleButtons>
       <BaseButton type={ButtonType.Icon} onClick={() => navigate(-1)} sx={{ borderRadius: 0 }}>
-        <Tooltip title='previous page'>
+        <Tooltip title='Previous page'>
           <ArrowBackIcon />
         </Tooltip>
       </BaseButton>
 
       <BaseButton type={ButtonType.Icon} onClick={() => navigate(1)} sx={Styled.LeftButton}>
-        <Tooltip title='next page'>
+        <Tooltip title='Next page'>
           <ArrowForwardIcon />
         </Tooltip>
       </BaseButton>
