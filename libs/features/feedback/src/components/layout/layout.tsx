78703ec32ecc0e59aa6c08e92f6c3cf3741afaaf
Reden4005 <olus81_81@o2.pl>
2022-03-31 18:54:39 +0200

    refactor(backlinkcomponent): add back-link-button to feedback layout

--- libs/features/feedback/src/components/layout/layout.tsx
+++ libs/features/feedback/src/components/layout/layout.tsx
@@ -1,12 +1,10 @@
-import { Layout as BasicLayout, PreviousPageButton } from "@patronage-web/shared";
+import { Layout as BasicLayout } from "@patronage-web/shared";
 
 import { footerConfig } from "./footer-config";
 import { navbarConfig } from "./navbar-config";
 
 export const Layout: React.FC = ({ children }) => (
   <BasicLayout navbarConfig={navbarConfig} footerConfig={footerConfig}>
-    <PreviousPageButton />
-
     {children}
   </BasicLayout>
 );
