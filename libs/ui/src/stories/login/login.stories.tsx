58655d73037c5b349bed844ef8163bc1db21f2d4
A.Wasilewski <37738097+Adys23@users.noreply.github.com>
2022-04-06 15:05:33 +0200

    JI:768392 P2022-1542 Summary of the finished presentation (#75)
    * feat(external presentation summary + storybook cleanup): add presentation summary component
    
    make changes in form and googleLogin stories to make them work again
    
    * refactor(presentation summary style): adjust display style
    
    * style(presentation summary): fix naming
    
    * chore(translations): correct misspelling
    
    * refactor(presentation live summary): change name of the component and adjust code based on CR
    
    * style(presentation live summary): remove array brackets from useTranslation

--- libs/ui/src/stories/login/login.stories.tsx
+++ libs/ui/src/stories/login/login.stories.tsx
@@ -1,7 +1,8 @@
-import { Login } from "@patronage-web/shared";
+/* eslint-disable @nrwl/nx/enforce-module-boundaries */
+import { GoogleLogin } from "@patronage-web/site";
 import { ComponentMeta } from "@storybook/react";
 
-export const LoginStory: React.FC = () => <Login />;
+export const LoginStory: React.FC = () => <GoogleLogin />;
 
 export default {
   title: "Login",
