91c12d1065659eff92d39f7fbb6145fd0424b7eb
piotrrawski <piotr.rawski@gmail.com>
2022-04-06 12:57:46 +0200

    refactor(dashboard): change presentation status type

--- libs/features/feedback/src/types/dashboard.ts
+++ libs/features/feedback/src/types/dashboard.ts
@@ -1,11 +1,16 @@
 import { CardProps } from "@mui/material";
 
+export enum PresentationStatus {
+  Done = "DONE",
+  Draft = "DRAFT",
+  Shared = "SHARED"
+}
 export interface Presentation {
   id: string;
   isPublic: boolean;
   title: string;
   description?: string;
-  status: string;
+  status: PresentationStatus;
 }
 export interface DashboardCardProps extends CardProps {
   isPublic: boolean;
