5f350c0fc2b6470e730dbb9402d21587ed4bac7d
Paweł Wojdas <paw.wojdas@gmail.com>
2022-04-12 10:53:18 +0200

    refactor(firebase config): refactor firebase config

--- libs/shared/src/configs/firebase.ts
+++ libs/shared/src/configs/firebase.ts
@@ -1,9 +1,9 @@
 import { verifyAuth } from "../utils";
 
-const apiKey = verifyAuth() ? process.env.NX_FIREBASE_API_KEY : "sample-api-key";
-const authDomain = verifyAuth() ? process.env.NX_FIREBASE_AUTH_DOMAIN : "sample-auth-domain";
+const getApiKey = () => (verifyAuth() ? process.env.NX_FIREBASE_API_KEY : "sample-api-key");
+const getAuthDomain = () => (verifyAuth() ? process.env.NX_FIREBASE_AUTH_DOMAIN : "sample-auth-domain");
 
-export const FIREBASE_CONFIG = {
-  apiKey,
-  authDomain
+export const firebaseConfig = {
+  apiKey: getApiKey(),
+  authDomain: getAuthDomain()
 };
--- libs/shared/src/services/firebase/firebase.ts
+++ libs/shared/src/services/firebase/firebase.ts
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-import { FIREBASE_CONFIG } from "../../configs";
+import { firebaseConfig } from "../../configs";
 
-export const firebaseInstance = initializeApp(FIREBASE_CONFIG);
+export const firebaseInstance = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(firebaseInstance);
