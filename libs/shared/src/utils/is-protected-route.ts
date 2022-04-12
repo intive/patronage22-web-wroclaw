7caa2c5a3e086ca91f5d364ca05fb6dba25ea1ba
Reden4005 <olus81_81@o2.pl>
2022-04-12 15:49:13 +0200

    refactor(isprotectedroute): changes according CR

--- libs/shared/src/utils/is-protected-route.ts
+++ libs/shared/src/utils/is-protected-route.ts
@@ -2,4 +2,4 @@ import { Location } from "react-router-dom";
 
 import { PROTECTED_ROUTES } from "../types/protected-routes";
 
-export const isProtectedRoute = (location: Location) => !!PROTECTED_ROUTES.includes(location.pathname);
+export const isProtectedRoute = (location: Location) => PROTECTED_ROUTES.includes(location.pathname);
