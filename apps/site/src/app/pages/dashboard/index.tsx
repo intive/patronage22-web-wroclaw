1081e8953cfff661524a6b6d55c3c5c48f866c21
piotrrawski <piotr.rawski@gmail.com>
2022-04-19 15:29:50 +0200

    feat(dashboard): add dashboard view to page

--- apps/site/src/app/pages/dashboard/index.tsx
+++ apps/site/src/app/pages/dashboard/index.tsx
@@ -1,8 +1,5 @@
-const Dashboard: React.FC = () => {
-  return (
-    // TODO - replace when proper data will be delivered
-    <h1>Dashboard</h1>
-  );
-};
+import { Dashboard as DashboardView } from "@patronage-web/features-feedback";
+
+const Dashboard: React.FC = () => <DashboardView />;
 
 export default Dashboard;
