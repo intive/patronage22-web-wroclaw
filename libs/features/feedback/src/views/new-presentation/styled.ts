2fa3ac7907697de7245fede0368ede949f3f5d74
Krystyna Łosieczka <krysia@agabox.net>
2022-04-19 16:02:23 +0200

    refactor(question view): pull latest develop

--- libs/features/feedback/src/views/new-presentation/styled.ts
+++ libs/features/feedback/src/views/new-presentation/styled.ts
@@ -3,7 +3,6 @@ import { Box, styled } from "@mui/material";
 export const NewPresentationWrapper = styled(Box)(({ theme }) => ({
   display: "flex",
   flexDirection: "column",
-  alignSelf: "flex-start",
   marginTop: theme.spacing(13),
   padding: theme.spacing(0, 2)
 }));
