1df450893277ce7b2388e8f5086b9befe52fce6d
piotrrawski <piotr.rawski@gmail.com>
2022-04-20 14:15:25 +0200

    refactor(dashboard): change mock data to search

--- libs/shared/src/data/feedback/mocks/presentation-search-items/index.ts
+++ libs/shared/src/data/feedback/mocks/presentation-search-items/index.ts
@@ -1,10 +1,72 @@
 import { PresentationSearchItem } from "../../../../components";
 
 export const items: PresentationSearchItem[] = [
-  { title: "Advanced typescript", id: "8ef796d1-b39d-4c4b-91a4-689534944d66" },
-  { title: "Javascript for begginers", id: "3d7812ee-43b5-43f3-8a3e-ea8938cbf8c1" },
-  { title: "Advanced React", id: "db5d7672-b143-4fa4-941c-5aa71e0eb575" },
-  { title: "Advanced Javascript", id: "1523a989-ceee-422a-b460-d5e2b842f742" },
-  { title: "Typescript for begginers", id: "acf31308-3432-4b08-a807-538d4f8e288d" },
-  { title: "React for begginers", id: "73bae9d6-3af9-4927-998b-605fc215dfac" }
+  {
+    id: "91151a02-8e93-11ec-b909-0242ac120002",
+    title: "Sample presentation name"
+  },
+  {
+    id: "fe6507a0-381a-402b-9513-cd232b0a7a23",
+    title: "Presentation 1 Java"
+  },
+  {
+    id: "79ca8de9-2a87-41a3-b872-b97b50afd7b2",
+    title: "Presentation 2 JavaScript"
+  },
+  {
+    id: "4455b691-0d09-4da9-a7ab-9644831d4e50",
+    title: "Presentation 3 Title"
+  },
+  {
+    id: "f2930528-7472-44e4-b815-d34ed09e1193",
+    title: "Presentation 4 React"
+  },
+  {
+    id: "a8fabad5-2f5b-433a-a2e8-ee94a56e306a",
+    title: "Presentation 5 Java"
+  },
+  {
+    id: "e50fdb9c-a052-4edf-b033-d4d3302a795e",
+    title: "Presentation 6 React"
+  },
+  {
+    id: "acfc1574-850d-4d7d-b8bd-b5db61ffb701",
+    title: "Presentation 7 JavaScript"
+  },
+  {
+    id: "4ac276dc-d5dc-4e5f-aab2-f88cea63d1e2",
+    title: "Presentation 8 React"
+  },
+  {
+    id: "50311cbf-dc02-440a-9704-2d500b6a018e",
+    title: "Presentation 9 JavaScript"
+  },
+  {
+    id: "a05db012-0d8c-4f4b-8900-0c13a5737686",
+    title: "Presentation 10 Title"
+  },
+  {
+    id: "2b8016cd-ea88-46bd-aa18-79286a7c675e",
+    title: "Presentation 11 Title"
+  },
+  {
+    id: "8ba6475e-2388-4c09-bcd0-4541f8a8c261",
+    title: "Presentation 12 Java"
+  },
+  {
+    id: "a7719af8-baaf-449f-ad6b-0901323bd629",
+    title: "Presentation 13 React"
+  },
+  {
+    id: "8cfe452b-4794-4bb9-a5fa-890e185a8ad1",
+    title: "Presentation 14 JavaScript"
+  },
+  {
+    id: "2112a8f9-dd07-4acd-992b-fc1ad3c06250",
+    title: "Presentation 15 Java"
+  },
+  {
+    id: "59dd33a0-dd0d-4ad5-9719-ff2c70e3083b",
+    title: "Presentation 16 React"
+  }
 ];
